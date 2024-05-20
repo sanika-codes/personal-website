01d214b72700454f764efce66986f79de6fa8cbb
Sanika <sanika@theamberhorizons.com>
2024-05-20 10:26:30 +0530

    Fixed feedback submission

--- server.js
+++ server.js
@@ -9,29 +9,31 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 app.post('/feedback', async function (req, res)  {
   try {
-    const  {feedback} = req.body; // Assuming you have a JSON request body with name and email fields
+    const  {positive, negative, comment} = req.body; // Assuming you have a JSON request body with name and email fields
    
     // SQL query to insert data into the table (replace with your table name and schema)
     const insertDataSQL = `
-      INSERT INTO feedback (feedback)
-      VALUES (?)
+      INSERT INTO feedback (positive, negative, comment)
+      VALUES (?,?,?)
     `;
 
     // Query the MySQL server to insert data into the table
-    const result = await db.query(insertDataSQL,[feedback]);
+    const result = await db.query(insertDataSQL,[positive, negative, comment]);
     
     if (result[0].affectedRows === 1) {
       res.status(200).json({ status : 'ok', message: 'Data inserted successfully' });
       
     } else {
       res.status(500).json({ error: 'Failed to insert data' });
-    }
+    } 
     
   } catch (error) {
     console.error('Error inserting data into the table:', error);
     res.status(500).json({ error: 'An error occurred while inserting data' });
   }
 });
+
+
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -15,7 +15,7 @@ function Intro() {
         My name is Sanika Patwardhan. I bring designs and ideas to (e-)life
         using modern front-end technologies like React.
       </p>
-      <button class="cta theme-button"><a href={myResume} target="_blank" rel="noreferrer">Download my Resume</a></button>
+      <button className="cta theme-button"><a href={myResume} target="_blank" rel="noreferrer">Download my Resume</a></button>
       </div>
      
     </div>
